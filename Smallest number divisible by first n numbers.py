22a0d51ffc529f43ca3f86b62afc2ca997cd3494
Vnd443 <59206444+Vnd443@users.noreply.github.com>
2020-05-05 14:04:05 +0530

    Update Smallest number divisible by first n numbers.py

--- Smallest number divisible by first n numbers.py
+++ Smallest number divisible by first n numbers.py
@@ -1,14 +1,14 @@
-# Returns the lcm of first n numbers 
+# https://www.youtube.com/watch?v=PP75FwAX9DI for explaination
+
 import fractions
     
 def getSmallestDivNum(n):
-    #RETURN ans
     ans = 1
     for i in range(1, n+1):
         print(ans,i,ans*i,fractions.gcd(ans,i))
 
         ans=(ans*i)//fractions.gcd(ans,i)
-        #print(ans,fractions.gcd(ans,i))
+       
 
     return int(ans)
 a=int(input("enter Smallest number divisible by first n numbers"))
